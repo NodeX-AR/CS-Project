98fcf7257045895bc270722fa29cbbe70f49c2cf
Aswanth R <aswanthr82@gmail.com>
2026-03-25 19:45:46 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,5 +1,4 @@
 import mysql.connector as sqlcon
-
 db=sqlcon.connect(
     host='localhost',
     user='root',
@@ -12,56 +11,63 @@ cursor=db.cursor()
 def registry():
     cursor.execute("Select * from list")
     data=cursor.fetchall()
+    print("|Index No    |      Name     |    Book     |    Status    |")
+    print("+---------------------------------------------------------+")
     for i in data:
         print(i)
 
 def new():
     n=input("Enter the name of new student:")
-    query = "INSERT INTO `list` (student_name) VALUES ('"+ n +"')"
-    cursor.execute(query)
+    query = "INSERT INTO `list` (student_name) VALUES (%s)"
+    cursor.execute(query,(n,))
     db.commit()
     print("Student",n,"is sucessfully added.")
 
 def breturn():
     n=input("Enter the name of new student:")
-    query = "UPDATE `list` SET status = 'Book Returned', book = NULL WHERE student_name = '"+n+"'"
-    cursor.execute(query)
+    query = "UPDATE `list` SET status = 'Book Returned', book = NULL WHERE student_name = %s"
+    cursor.execute(query,(n,))
     db.commit()
     print("Student",n," has retured the book. Status Updated !")
 
 def issue():
     n=input("Enter the name of new student:")
-    query = "SELECT status FROM `list` WHERE student_name = '"+n+"'"
-    cursor.execute(query)
+    query = "SELECT status FROM `list` WHERE student_name = %s"
+    cursor.execute(query,(n,))
     result = cursor.fetchone()
-    if result == None:
+    if result is None:
         print("Student not found.")
         return
+    
     elif result[0] == "Book Issued":
         print("The student",n,"has already been issued a book.")
         return
+    
     b=input("Enter the book name:")
-    query = "UPDATE `list` SET status = 'Book Issued', book = '"+b+"' WHERE student_name = '"+n+"'"
-    cursor.execute(query)
+    query = "UPDATE `list` SET status = 'Book Issued', book = %s WHERE student_name = %s"
+    cursor.execute(query,(b,n))
     db.commit()
-    print("Book ",b," issued to ",n," successfully!")
+    print("Book",b,"issued to",n,"successfully!")
 
 def status():
     n=input("Enter the name of new student:")
-    query = "SELECT status,book FROM `list` WHERE student_name = '"+n+"'"
-    cursor.execute(query)
+    query = "SELECT status,book FROM `list` WHERE student_name = %s"
+    cursor.execute(query,(n,))
     result = cursor.fetchone()
-    if result == None:
+    if result is None:
         print("Student not found.")
+        
     elif result[0] == "Book Issued":
         print("The student",n,"has been issued a book ")
         print("The book issued is ",result[1])
         return
+    
     elif result[0] == "Book Returned":
         print("The student",n,"has already returned the book.")
+        
     else:
         print("The student",n,"has never taken a book.")
-      
+        
 def main():
     print("+---------------------------------------------------------+")
     print("|               LIBRARY MANAGEMENT SYSTEM                 |")
@@ -80,21 +86,28 @@ def main():
         print("+---------------------------------------------------------+")
         if opt == "1" :
             break
+        
         elif opt == "2":
             registry()
+            
         elif opt == "3":
             new()
+            
         elif opt == "4":
             breturn()
+            
         elif opt == "5":
             issue()
+            
         elif opt == "6":
             status()
+            
         else:
             print("Please enter a valid option")
             continue
-    print("|            Thank You using our program                  |")
+    print("             Thank You using our program                  |")
     print("+---------------------------------------------------------+")
     db.close()
           
-main()
+if __name__ == "__main__":
+    main()
